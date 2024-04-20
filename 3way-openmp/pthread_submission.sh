57a40a98db03c1eb0bcfa2b951942ba2375fe615
Gabriel Whitehair <gmwhitehair@ksu.edu>
2024-04-19 22:08:39 -0500

    Changes so far, still problems

---
+++ 3way-openmp/pthread_submission.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Run make to build the executable
+make clean
+make
+
+# Create a batch script for sbatch commands
+cat <<EOF > job_script.sh
+#!/bin/sh
+
+# Run the executable
+./max_ascii_values
+
+# You can add more commands here if needed
+
+EOF
+
+# Make the batch script executable
+chmod u+x job_script.sh
+
+# Submit the batch script using sbatch
+for i in 1 5 10 20
+do
+
+    sbatch --constraint=moles --time=02:00:00 --mem-per-cpu=$((20/$i))G --cpus-per-task=$i --ntasks=1 --nodes=1 ./job_script.sh
+done
